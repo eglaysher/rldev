7dc2c9459d303b1fe8bf675da6a48fd23d793a61
Haeleth <haeleth@haeleth.net>
2006-04-22 21:19:56 +0000

    RLdev: try to fix memory-related discrepancies by adding GC wrappers in C files
    git-svn-id: http://svn.haeleth.net/pub/rldev@91 8d5b77e3-600c-0410-b16e-99778ea16b4f

--- src/common/binarray-c.c
+++ src/common/binarray-c.c
@@ -33,22 +33,22 @@
 
 CAMLprim value string_of_binary_array (value src_arr, value src_idx, value src_len)
 {
-  CAMLparam3 (src_arr, src_idx, src_len);
-  CAMLlocal1 (retval);
-  int len = Long_val(src_len);
-  retval = alloc_string (len);
-  memmove (String_val(retval), (char *) Data_bigarray_val(src_arr) + (Long_val(src_idx)), len);
-  CAMLreturn (retval);
+	CAMLparam3 (src_arr, src_idx, src_len);
+	CAMLlocal1 (retval);
+	int len = Long_val(src_len);
+	retval = alloc_string (len);
+	memmove (String_val(retval), (char *) Data_bigarray_val(src_arr) + (Long_val(src_idx)), len);
+	CAMLreturn (retval);
 }
 
 
 /* Fill a buffer from a slice of a Binarary.t */
 CAMLprim value string_from_binary_array (value src_arr, value buffer, value src_idx, value src_len, value dst_idx)
 {
-  CAMLparam5 (src_arr, buffer, src_idx, src_len, dst_idx);
-  char *dst = (String_val(buffer)) + (Long_val(dst_idx));
-  memmove (dst, (char *) Data_bigarray_val(src_arr) + (Long_val(src_idx)), Long_val(src_len));
-  CAMLreturn (Val_unit);
+	CAMLparam5 (src_arr, buffer, src_idx, src_len, dst_idx);
+	char *dst = (String_val(buffer)) + (Long_val(dst_idx));
+	memmove (dst, (char *) Data_bigarray_val(src_arr) + (Long_val(src_idx)), Long_val(src_len));
+	CAMLreturn (Val_unit);
 }
 
 
@@ -56,32 +56,32 @@ CAMLprim value string_from_binary_array (value src_arr, value buffer, value src_
 
 CAMLprim value cstring_of_binary_array (value src_arr, value src_idx, value src_len)
 {
-  CAMLparam3 (src_arr, src_idx, src_len);
-  CAMLlocal1 (retval);
-  int len = Long_val(src_len);
-  char *retstr = (char *) calloc (len + 1, 1);
-  if (retstr == NULL) failwith ("unable to allocate internal buffer");
-  memcpy (retstr, (char *) Data_bigarray_val(src_arr) + (Long_val(src_idx)), len);
-  retval = copy_string (retstr);
-  free (retstr);
-  CAMLreturn (retval);
+	CAMLparam3 (src_arr, src_idx, src_len);
+	CAMLlocal1 (retval);
+	int len = Long_val(src_len);
+	char *retstr = (char *) calloc (len + 1, 1);
+	if (retstr == NULL) failwith ("unable to allocate internal buffer");
+	memcpy (retstr, (char *) Data_bigarray_val(src_arr) + (Long_val(src_idx)), len);
+	retval = copy_string (retstr);
+	free (retstr);
+	CAMLreturn (retval);
 }
 
 /* As above, but unsafe; the advantage is that you don't need to know how long it might be. */
 CAMLprim value unsafe_cstring_of_binary_array (value src_arr, value src_idx)
 {
-  return copy_string ((char *) Data_bigarray_val(src_arr) + (Int_val(src_idx)));
+	return copy_string ((char *) Data_bigarray_val(src_arr) + (Int_val(src_idx)));
 }
 
 /* Write a string to a Binarray.t */
 
 CAMLprim value string_to_binary_array (value dst_arr, value dst_idx, value src_str)
 {
-  CAMLparam3 (dst_arr, dst_idx, src_str);
-  int len = string_length (src_str), idx = Long_val(dst_idx);
-  if (idx + len > Bigarray_val(dst_arr)->dim[0]) invalid_argument ("Binarray.write");
-  memcpy ((char *) Data_bigarray_val(dst_arr) + idx, String_val(src_str), len);
-  CAMLreturn (Val_unit);
+	CAMLparam3 (dst_arr, dst_idx, src_str);
+	int len = string_length (src_str), idx = Long_val(dst_idx);
+	if (idx + len > Bigarray_val(dst_arr)->dim[0]) invalid_argument ("Binarray.write");
+	memcpy ((char *) Data_bigarray_val(dst_arr) + idx, String_val(src_str), len);
+	CAMLreturn (Val_unit);
 }
 
 
@@ -89,21 +89,21 @@ CAMLprim value string_to_binary_array (value dst_arr, value dst_idx, value src_s
 
 CAMLprim value cstring_to_binary_array (value dst_arr, value dst_idx, value dst_len, value src_str)
 {
-  CAMLparam4 (dst_arr, dst_idx, dst_len, src_str);
-  int len = string_length (src_str), idx = Long_val(dst_idx), dlen = Long_val(dst_len);
-  if (idx + dlen > Bigarray_val(dst_arr)->dim[0] || len > dlen) invalid_argument ("Binarray.write_sz");
-  memcpy ((char *) Data_bigarray_val(dst_arr) + idx, String_val(src_str), len);
-  memset ((char *) Data_bigarray_val(dst_arr) + idx + len, 0, dlen - len);
-  CAMLreturn (Val_unit);
+	CAMLparam4 (dst_arr, dst_idx, dst_len, src_str);
+	int len = string_length (src_str), idx = Long_val(dst_idx), dlen = Long_val(dst_len);
+	if (idx + dlen > Bigarray_val(dst_arr)->dim[0] || len > dlen) invalid_argument ("Binarray.write_sz");
+	memcpy ((char *) Data_bigarray_val(dst_arr) + idx, String_val(src_str), len);
+	memset ((char *) Data_bigarray_val(dst_arr) + idx + len, 0, dlen - len);
+	CAMLreturn (Val_unit);
 }
 
 
 /* MD5 digest of a Binarray.t */
 
 typedef struct {
-  uint32 buf[4];
-  uint32 bits[2];
-  unsigned char in[64];
+	uint32 buf[4];
+	uint32 bits[2];
+	unsigned char in[64];
 } MD5Context;
 
 CAMLextern void MD5Init (MD5Context *context);
@@ -112,21 +112,21 @@ CAMLextern void MD5Final (unsigned char *digest, MD5Context *ctx);
 
 CAMLprim value digest_array (value v_iarr)
 {
-  CAMLparam1(v_iarr);
-  CAMLlocal1(result);
-  MD5Context context;
-  int len = Bigarray_val(v_iarr)->dim[0];
-  unsigned char *buf = Data_bigarray_val(v_iarr);
+	CAMLparam1(v_iarr);
+	CAMLlocal1(result);
+	MD5Context context;
+	int len = Bigarray_val(v_iarr)->dim[0];
+	unsigned char *buf = Data_bigarray_val(v_iarr);
 
-  MD5Init (&context);
-  while (len > 0) {
-    int block = (len > 8192) ? 8192 : len;
-    MD5Update (&context, buf, block);
-    buf += block;
-    len -= block;
-  }
+	MD5Init (&context);
+	while (len > 0) {
+		int block = (len > 8192) ? 8192 : len;
+		MD5Update (&context, buf, block);
+		buf += block;
+		len -= block;
+	}
 
-  result = alloc_string (16);
-  MD5Final (&Byte_u(result, 0), &context);
-  CAMLreturn(result);
+	result = alloc_string (16);
+	MD5Final (&Byte_u(result, 0), &context);
+	CAMLreturn(result);
 }
